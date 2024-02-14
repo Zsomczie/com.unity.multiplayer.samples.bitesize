480a5ed1a27eaa3feac5bc12c561018fea0c1ce3
Zsomczie <zsomi.f@gmail.com>
2024-02-14 08:38:34 +0200

    pooling

--- Basic/ClientDriven/Assets/Scripts/ZsomborTest/ObjectPooling.cs
+++ Basic/ClientDriven/Assets/Scripts/ZsomborTest/ObjectPooling.cs
@@ -1,26 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Unity.Netcode;
+using UnityEngine;
 
-public class ObjectSpawner : NetworkBehaviour
+public class ObjectPooling : NetworkBehaviour
 {
     [SerializeField] GameObject Spawner;
     [SerializeField] GameObject Prefab;
+    [SerializeField] List<GameObject> PooledObjects;
     [SerializeField] float spawnRate;
     bool spawnerEnabled;
-    // Update is called once per frame
+    private void Start()
+    {
+        PooledObjects = new List<GameObject>();
+    }
     void FixedUpdate()
     {
-        if (IsServer&&!spawnerEnabled)
+        if (IsServer && !spawnerEnabled)
         {
             InvokeRepeating(nameof(SpawnObject), 2f, spawnRate);
             spawnerEnabled = true;
+
+        }
+        if (PooledObjects.Count>3&& !PooledObjects[PooledObjects.Count - 1].activeSelf) 
+        {
+            PooledObjects[PooledObjects.Count-1].SetActive(true);
         }
     }
-    void SpawnObject() 
+    void SpawnObject()
     {
         var instance = Instantiate(Prefab, Spawner.transform.position, Spawner.transform.rotation);
         instance.GetComponent<NetworkObject>().Spawn();
+        instance.SetActive(false);
+        PooledObjects.Add(instance);
     }
 }
