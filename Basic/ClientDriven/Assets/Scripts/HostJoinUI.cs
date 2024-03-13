a3b066862ec7cadbdbeeac64d9b30c58a1d0aa09
Zsomczie <zsomi.f@gmail.com>
2024-03-13 10:26:32 +0200

    demo8

--- Basic/ClientDriven/Assets/Scripts/HostJoinUI.cs
+++ Basic/ClientDriven/Assets/Scripts/HostJoinUI.cs
@@ -1,12 +1,26 @@
 using System;
+using System.Net.Sockets;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class HostJoinUI : MonoBehaviour
 {
+    [SerializeField]
+    int maxConnections = 5;
+    [SerializeField]
+    bool usingUnityRelay = false;
+    [SerializeField]
+    string joinCode = null;
+
     [SerializeField]
     UIDocument m_MainMenuUIDocument;
 
@@ -14,30 +28,30 @@ public class HostJoinUI : MonoBehaviour
     UIDocument m_InGameUIDocument;
 
     VisualElement m_MainMenuRootVisualElement;
-    
+
     VisualElement m_InGameRootVisualElement;
-    
+
     Button m_HostButton;
-    
+
     Button m_ServerButton;
-    
+
     Button m_ClientButton;
 
     TextField m_IPAddressTextField;
-    
+
     TextField m_PortTextField;
-    
+
     void Awake()
     {
         m_MainMenuRootVisualElement = m_MainMenuUIDocument.rootVisualElement;
         m_InGameRootVisualElement = m_InGameUIDocument.rootVisualElement;
-        
+
         m_HostButton = m_MainMenuRootVisualElement.Query<Button>("HostButton");
         m_ClientButton = m_MainMenuRootVisualElement.Query<Button>("ClientButton");
         m_ServerButton = m_MainMenuRootVisualElement.Query<Button>("ServerButton");
         m_IPAddressTextField = m_MainMenuRootVisualElement.Query<TextField>("IPAddressField");
         m_PortTextField = m_MainMenuRootVisualElement.Query<TextField>("PortField");
-        
+
         m_HostButton.clickable.clickedWithEventInfo += StartHost;
         m_ServerButton.clickable.clickedWithEventInfo += StartServer;
         m_ClientButton.clickable.clickedWithEventInfo += StartClient;
@@ -54,12 +68,39 @@ public class HostJoinUI : MonoBehaviour
     {
         ToggleMainMenuUI(true);
         ToggleInGameUI(false);
+
+        // !!!!!!!!
+        // Modified for the Relay service usage
+        // Checking the used UnityTransport Protocol type
+        var transport = NetworkManager.Singleton.NetworkConfig.NetworkTransport;
+
+        if (transport is UnityTransport unityTransport && unityTransport.Protocol.Equals(UnityTransport.ProtocolType.RelayUnityTransport))
+        {
+            usingUnityRelay = true;
+        }
+
     }
 
+    // !!!!!!!!
+    // Modified to accept Relay service
     void StartHost(EventBase obj)
     {
-        SetUtpConnectionData();
-        var result = NetworkManager.Singleton.StartHost();
+        var result = false;
+        if (usingUnityRelay)
+        {
+            var joinCodeTask = StartHostWithRelay(maxConnections);
+            if (joinCodeTask != null)
+            {
+                print("THE JOIN CODE: " + joinCode);
+                result = true;
+            }
+        }
+        else
+        {
+            SetUtpConnectionData();
+            result = NetworkManager.Singleton.StartHost();
+
+        }
         if (result)
         {
             ToggleInGameUI(true);
@@ -67,15 +108,68 @@ public class HostJoinUI : MonoBehaviour
         }
     }
 
+    // !!!!!!!!
+    // Added method for starting a host while using Relay service, max connections defined in this script (would be better to have closer to NetworkManager)
+    public async Task<string> StartHostWithRelay(int maxConnections = 5)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "udp"));// "dtls"));
+        var receivedJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        joinCode = receivedJoinCode.ToString();
+        var scoreUI = FindObjectOfType<ScoreUI>();
+        scoreUI.OnJoinCodeReceived(joinCode);
+
+        return NetworkManager.Singleton.StartHost() ? receivedJoinCode : null;
+    }
+
+    // !!!!!!!!
+    // Modified to accept Relay service
     void StartClient(EventBase obj)
     {
-        SetUtpConnectionData();
-        var result = NetworkManager.Singleton.StartClient();
+        var result = false;
+        if (usingUnityRelay)
+        {
+            // Join code typed to the IP field if using Unity Relay as the UnityTransport Protocol Type
+            joinCode = Sanitize(m_IPAddressTextField.text);
+            var joined = StartClientWithRelay(joinCode);
+            if (joined != null)
+            {
+                print("Joined in!: " + joinCode);
+                result = true;
+            }
+        }
+        else
+        {
+            SetUtpConnectionData();
+            result = NetworkManager.Singleton.StartClient();
+        }
+
         if (result)
         {
             ToggleInGameUI(true);
             ToggleMainMenuUI(false);
         }
+    }
+
+    // !!!!!!!!
+    // Added method for starting a client when using Relay service
+    public async Task<bool> StartClientWithRelay(string joinCode)
+    {
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
     }
 
     void StartServer(EventBase obj)
@@ -93,7 +187,7 @@ public class HostJoinUI : MonoBehaviour
     {
         m_MainMenuRootVisualElement.style.display = isVisible ? DisplayStyle.Flex : DisplayStyle.None;
     }
-    
+
     void ToggleInGameUI(bool isVisible)
     {
         m_InGameRootVisualElement.style.display = isVisible ? DisplayStyle.Flex : DisplayStyle.None;
@@ -105,11 +199,11 @@ public class HostJoinUI : MonoBehaviour
         var sanitizedPortText = Sanitize(m_PortTextField.text);
 
         ushort.TryParse(sanitizedPortText, out var port);
-        
+
         var utp = (UnityTransport)NetworkManager.Singleton.NetworkConfig.NetworkTransport;
         utp.SetConnectionData(sanitizedIPText, port);
     }
-    
+
     /// <summary>
     /// Sanitize user port InputField box allowing only alphanumerics and '.'
     /// </summary>
--- Basic/ClientDriven/Assets/Scripts/ScoreUI.cs
+++ Basic/ClientDriven/Assets/Scripts/ScoreUI.cs
@@ -5,35 +5,54 @@ using UnityEngine.UIElements;
 public class ScoreUI : MonoBehaviour
 {
     [SerializeField]
-    ServerScoreReplicator m_ScoreTracker;
+    ServerScoreReplicator m_ScoreTracker1;
+    [SerializeField]
+    ServerScoreReplicator m_ScoreTracker2;
+    //[SerializeField] System.Collections.Generic.List<ServerScoreReplicator> m_ScoreTrackerList; //WIP: more generalised score tracker system
 
     [SerializeField]
     UIDocument m_InGameUIDocument;
-    
+
     VisualElement m_InGameRootVisualElement;
-    
-    TextElement m_ScoreText;
+
+    TextElement m_ScoreText1;
+    TextElement m_ScoreText2;
+
+    TextElement m_JoinCodeText;
 
     void Awake()
     {
         m_InGameRootVisualElement = m_InGameUIDocument.rootVisualElement;
-        
-        m_ScoreText = m_InGameRootVisualElement.Query<TextElement>("ScoreText");
+
+        m_ScoreText1 = m_InGameRootVisualElement.Query<TextElement>("Score1Text");
+        m_ScoreText2 = m_InGameRootVisualElement.Query<TextElement>("Score2Text");
+        m_JoinCodeText = m_InGameRootVisualElement.Query<TextElement>("JoinCodeText");
     }
 
     void Start()
     {
-        OnScoreChanged(0, m_ScoreTracker.ReplicatedScore.Value);
-        m_ScoreTracker.ReplicatedScore.OnValueChanged += OnScoreChanged;
+        OnScoreChanged(0, m_ScoreTracker1.ReplicatedScore.Value);
+        m_ScoreTracker1.ReplicatedScore.OnValueChanged += OnScoreChanged;
+
+        OnScoreChanged(0, m_ScoreTracker2.ReplicatedScore.Value);
+        m_ScoreTracker2.ReplicatedScore.OnValueChanged += OnScoreChanged;
     }
 
     void OnDestroy()
     {
-        m_ScoreTracker.ReplicatedScore.OnValueChanged -= OnScoreChanged;
+        m_ScoreTracker1.ReplicatedScore.OnValueChanged -= OnScoreChanged;
+
+        m_ScoreTracker2.ReplicatedScore.OnValueChanged -= OnScoreChanged;
     }
 
     void OnScoreChanged(int previousValue, int newValue)
     {
-        m_ScoreText.text = $"{m_ScoreTracker.Score}";
+        m_ScoreText1.text = $"{m_ScoreTracker1.Score}";
+        m_ScoreText2.text = $"{m_ScoreTracker2.Score}";
+    }
+
+    public void OnJoinCodeReceived(string newValue)
+    {
+        m_JoinCodeText.text = newValue;
     }
 }
